ddc96a44042abcc5e38368eb6c966c4302dd2ab2
Paul Bredenberg <paulbredenberg@gmail.com>
2018-05-27 03:50:25 -0400

    build latest (logo and link)

--- public/styles.js
+++ public/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/geoff/code/personal/paulandgeoff/client/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /Users/paulbredenberg/projects/paulandgeoff/client/src/styles.css */"./src/styles.css");
 
 
 /***/ })
