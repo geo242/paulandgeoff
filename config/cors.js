49a9841542f62085e7003eabaefb6ab13b50ca42
Geoff Tripoli <gtripoli@gmail.com>
2018-09-01 21:18:48 -0700

    Changed cors url

--- config/cors.js
+++ config/cors.js
@@ -5,7 +5,7 @@
  */
 
 module.exports = function corsMiddleware(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.NODE_ENV === 'production' ? 'https://paul-and-geoff-staging.herokuapp.com' : 'http://localhost:9000');
+  res.header("Access-Control-Allow-Origin", process.env.NODE_ENV === 'production' ? 'https://paulandgeoff.com' : 'http://localhost:9000');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true);
   next();
