d4d21125a299d90da3348eb388f4f0c24ba7f8cc
Geoff Tripoli <gtripoli@gmail.com>
2020-11-11 17:54:21 -0500

    fix: db connection information

--- config/environment/development.js
+++ config/environment/development.js
@@ -6,6 +6,6 @@ module.exports = {
   env: process.env.NODE_ENV,
   host: 'localhost',
   port: 9000,
-  mongoUser: 'server',
-  mongoUri: 'mongodb://ds263590.mlab.com:63590/heroku_pkggg76g'
+  mongoUser: 'admin',
+  mongoUri: 'mongodb+srv://cluster0.51uel.mongodb.net/paulandgeoff?retryWrites=true&w=majority'
 };
